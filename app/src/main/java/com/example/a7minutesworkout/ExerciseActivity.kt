11b53020fd5c78183df1b8fdd704cd62a7ac15d3
Rishabh Gupta <rishabh9aug@gmail.com>
2022-12-02 17:45:56 +0530

    first commit

---
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -0,0 +1,4 @@
+package com.example.a7minutesworkout
+
+class ExerciseActivity {
+}
--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -1,11 +1,41 @@
   package com.example.a7minutesworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.example.a7minutesworkout.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+  class MainActivity : AppCompatActivity() {
+
+      private var binding: ActivityMainBinding?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding?.root)
+
+//        val fstartbutton:FrameLayout=findViewById(R.id.fstart)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding?.root)
+
+        binding?.fstart?.setOnClickListener{
+
+            Toast.makeText(this,"hello",Toast.LENGTH_SHORT).show()
+//            val intent= Intent(this,ExerciseActivity::class.java)
+//            startActivity(intent)
+        }
+
+//        fstartbutton.setOnClickListener {
+//            Toast.makeText(this,"here we will start the exercise",Toast.LENGTH_SHORT).show()
+//        }
+
     }
+      override fun onDestroy() {
+          super.onDestroy()
+          binding=null
+      }
 }
