d94a93bd57d41f5cf7ab5dd464ce6e76ef5ab6ab
Rishabh Gupta <rishabh9aug@gmail.com>
2022-12-02 18:56:14 +0530

    commit after toolbar

--- app/src/main/java/com/example/a7minutesworkout/excercise_activity.kt
+++ app/src/main/java/com/example/a7minutesworkout/excercise_activity.kt
@@ -2,10 +2,16 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a7minutesworkout.databinding.ActivityExcerciseBinding
 
 class excercise_activity : AppCompatActivity() {
+
+    private var binding:ActivityExcerciseBinding?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_excercise)
+       binding=ActivityExcerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.toolbarExcercise)
     }
 }
