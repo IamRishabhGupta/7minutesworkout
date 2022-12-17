22e8bf5af806e25a40c81db52c91c7601bb39271
Rishabh Gupta <rishabh9aug@gmail.com>
2022-12-17 08:33:50 +0530

    added timer without progressbar (self)

--- app/src/main/java/com/example/a7minutesworkout/excercise_activity.kt
+++ app/src/main/java/com/example/a7minutesworkout/excercise_activity.kt
@@ -2,9 +2,17 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.a7minutesworkout.databinding.ActivityExcerciseBinding
 
 class excercise_activity : AppCompatActivity() {
+
+
+    private var countDownTimer:CountDownTimer?=null
+    private var timeDuration:Long=60000
+    private var pauseOffset:Long=0
+
 
     private var binding:ActivityExcerciseBinding?=null
 
@@ -22,5 +30,21 @@ class excercise_activity : AppCompatActivity() {
         binding?.toolbarExcercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        binding?.tvTimer?.text="${(timeDuration?.div(1000)).toString()}"
+
+        object : CountDownTimer(timeDuration - pauseOffset, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                binding?.tvTimer?.setText("${(millisUntilFinished) / 1000}")
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@excercise_activity,"timer is finished",Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+
+
+
     }
 }
